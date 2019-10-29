7002158266b98b0ec33ddd9a90b99d13efed7ff1
Eli Reiners <goelyukim@gmail.com>
2019-10-29 12:38:19 -0400

    added function stubs

---
+++ index.js
@@ -0,0 +1,39 @@
+function renderStartWindow() {
+
+};
+
+function handleQuestionAnsRendering() {
+
+};
+
+function handleNumRemaing(){
+
+};
+
+function handleNumCorrect(){
+
+};
+
+function handleCorrect() {
+
+};
+
+function handleWrong() {
+
+};
+
+function handleFinishWindow(){
+
+};
+
+function handleQuizApp(){
+    handleStartWindow();
+    handleQuestionAnsRendering();
+    handleNumRemaing();
+    handleNumCorrect();
+    handleCorrect();
+    handleWrong();
+    handleFinishWindow();
+}
+
+$(handleQuizApp);
